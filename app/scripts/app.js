9c09e4bd426271830e02abf8d9301e9be608ee35
Cliff <cliffmin@gmail.com>
2014-04-07 19:19:13 -0600

    front page done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,23 @@
 'use strict';
 
 angular
-  .module('3dPokemonApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('3dPokemonApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/new-pokemon', {
+                templateUrl: 'views/new-pokemon.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('3dPokemonApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.pokemon = [
+            {
+                name: "Pikachu",
+                url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTu2IWz_od01mH0W5eUkuMDwHESetUdCZjlPi5g6O_OHneQowJX"
+            },
+            {
+                name: "Ugly Thing",
+                url: "http://i1.cdnds.net/13/24/618x497/gaming-pokemon-x-and-y-artwork-3.jpg"
+
+            }
+        ]
+    });
